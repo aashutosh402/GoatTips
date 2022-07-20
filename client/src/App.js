d0cac5f1b3270c339fc00d1374030a3ab1d42686
aashutosh402 <chandrawanshiaashutosh@gmail.com>
2022-07-20 10:59:44 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {Route,Routes,Navigate} from  "react-router-dom"
+import { Main } from "./components/Main/Main";
+import { Signup } from './components/Signup/Signup';
+import { Login } from './components/Login/Login';
+
+
+function App() {
+  const user = localStorage.getItem("token")
+  return (
+    <Routes>
+<Route path="/" exact element={<Main/>}/>
+     <Route path="/signup" exact element={<Signup/>}/>
+     <Route path="/login" exact element={<Login/>}/>
+     {/* <Route path="/" exact element={<Navigate replace to = "/login"/>}/> */}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom"
+import { Signup } from './components/Signup/Signup';
+import { Login } from './components/Login/Login';
+import { Provider } from 'react-redux';
+import store from "./store"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+<BrowserRouter>
+   <Provider store = {store}>
+<App/>
+
+   </Provider>
+</BrowserRouter>
+
+);
+
+
+
